2848e537295da9db90a1099055ea3b4095193bf8
Shashank <sr22@stanford.edu>
2020-11-11 04:09:52 -0800

    refactored code, added json config files

--- project/cifar_classifier.py
+++ project/cifar_classifier.py
@@ -1,20 +1,14 @@
 from pytorch_lightning import Trainer, seed_everything
 import pytorch_lightning as pl
-from model import JeopardyModel
 import torch.nn as nn
+from model_im_q_a import JeopardyModel2
+from model import JeopardyModel
 from torchvision import transforms, datasets
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data import DataLoader
-from torch.optim import Adam, SGD
+from torch.optim import Adam
 from pytorch_lightning.loggers import WandbLogger
 
-# Steps:
-# 1. test with existing checkpoint, and we can see what kind of accuracy we are getting
-# checkpoint path is an argument
-
-wandb_logger = WandbLogger(name='testing_the_representation_with_SGD_50_epochs_starting_from_SGD_3q915wgy',project='contra-pytorchlightning')
-
-PATH = "/mnt/fs5/shashank2000/contra-pytorchlightning/kosxmjr3/checkpoints/epoch=199.ckpt"
 image_transforms = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
@@ -22,39 +16,44 @@ image_transforms = transforms.Compose([
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
+
 class CIFAR10Classifier(pl.LightningModule):
     
-    def __init__(self, jeop_model_path, learning_rate=0.03):
+    def __init__(self, vocab_sz, jeop_model_path, model_type, parent_config, config):
         # seems like I need to remember the vocab size for some reason?
         super().__init__()
-        self.learning_rate = learning_rate
-        self.save_hyperparameters()
-        self.main_model = JeopardyModel.load_from_checkpoint(jeop_model_path, vocab_sz=20541)
-        self.main_model.freeze() # make sure this works..
+    
+        if model_type == "inv":
+            self.main_model == JeopardyModel2.load_from_checkpoint(jeop_model_path, vocab_sz=vocab_sz, config=parent_config)
+        else:
+            self.main_model = JeopardyModel.load_from_checkpoint(jeop_model_path, vocab_sz=vocab_sz, config=parent_config)
+    
+        self.main_model.freeze()
         self.picture_model = self.main_model.image_feature_extractor
         self.fine_tune = nn.Linear(128, 10) # logistic regression
-        self.train_accuracy = pl.metrics.Accuracy()
         self.test_accuracy =  pl.metrics.Accuracy()
         self.val_accuracy = pl.metrics.Accuracy()
-        self.dataloaders = cifar10()
+
+        op = config.optim_params
+        self.dataloaders = cifar10(op.batch_size)
+        self.learning_rate = op.learning_rate
 
     def forward(self, x):
         x = self.picture_model(x)
-        return self.fine_tune(x)
+        x = self.fine_tune(x)
+        return nn.functional.log_softmax(x, dim=1)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
-        loss = nn.functional.cross_entropy(logits, y)
+        loss = nn.functional.nll_loss(logits, y)
         self.log('train_loss', loss)
-        acc = self.train_accuracy(logits, y) 
-        self.log('train_acc', acc)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
-        loss = nn.functional.cross_entropy(logits, y)
+        loss = nn.functional.nll_loss(logits, y)
         self.log('val_loss', loss)
         acc = self.val_accuracy(logits, y) 
         self.log('val_acc', acc)
@@ -64,7 +63,7 @@ class CIFAR10Classifier(pl.LightningModule):
         # also calculate accuracy
         x, y = batch
         logits = self(x)
-        loss = nn.functional.cross_entropy(logits, y)
+        loss = nn.functional.nll_loss(logits, y)
         acc = self.test_accuracy(logits, y) 
         self.log('test_acc', acc)
         self.log('test_loss', loss)
@@ -78,9 +77,6 @@ class CIFAR10Classifier(pl.LightningModule):
     
     def test_dataloader(self):
         return self.dataloaders[2]
-    
-    def training_epoch_end(self, outs):
-        self.log('train_acc_epoch', self.train_accuracy.compute())
 
     def validation_epoch_end(self, outs):
         self.log('val_acc_epoch', self.val_accuracy.compute())
@@ -89,10 +85,10 @@ class CIFAR10Classifier(pl.LightningModule):
         self.log('test_acc_epoch', self.test_accuracy.compute())
 
     def configure_optimizers(self):
-        return SGD(self.parameters(), lr=self.learning_rate, momentum=0.9, weight_decay=1e-4)
+        return Adam(self.parameters(), lr=self.learning_rate)
 
     
-def cifar10():
+def cifar10(bs):
     train_dataset = datasets.CIFAR10(
         root="/data5/wumike/cifar10",
         train=True,
@@ -104,35 +100,14 @@ def cifar10():
     train_indices, val_indices = indices[split:], indices[:split]
     train_sampler, val_sampler = SubsetRandomSampler(train_indices), SubsetRandomSampler(val_indices)
 
-    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=16, num_workers=48)
-    val_dataloader = DataLoader(train_dataset, sampler=val_sampler, batch_size=16, num_workers=48)
+    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=bs, num_workers=48)
+    val_dataloader = DataLoader(train_dataset, sampler=val_sampler, batch_size=bs, num_workers=48)
 
     test_dataset = datasets.CIFAR10(
         root="/data5/wumike/cifar10",
         train=False,
         transform=image_transforms
     )
-    test_dataloader = DataLoader(test_dataset, batch_size=16, num_workers=48)
+    test_dataloader = DataLoader(test_dataset, batch_size=bs, num_workers=48)
 
     return train_dataloader, val_dataloader, test_dataloader
-
-
-def test_lit_classifier(checkpoint=PATH):
-    # collect args here and pass it into the model
-    model = CIFAR10Classifier(checkpoint)
-    
-    trainer = Trainer(resume_from_checkpoint="/mnt/fs5/shashank2000/contra-pytorchlightning/3q915wgy/checkpoints/epoch=20.ckpt", profiler="simple", logger=wandb_logger, max_epochs=50, default_root_dir="/mnt/fs5/shashank2000/", gpus=[9])
-    trainer.fit(model)
-    results = trainer.test()
-    print(results[0]['test_acc'])
-    assert results[0]['test_acc'] > 0.7
-
-# find args, pass it in here
-# find args, pass it in here
-import sys
-# Pass in the filepath of the checkpoint of the trained VQA/Jeopardy model that you'd like to test.
-if len(sys.argv) > 2:
-    # no exception handling with invalid path, assuming that's okay
-    test_lit_classifier(sys.argv[2])
-else:
-    test_lit_classifier()
